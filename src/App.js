ca958e872d465ac6058c3909daf5a087ff12e171
ibrahim <ibrahimdonmezm@gmail.com>
2022-09-04 18:07:12 +0300

    kozmetik değişiklikler yapıldı

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
 
   return (
         <>
-          <section className="container">
-            <h3> Renk Dönüştürücü</h3>
+          <section className="container d-flex justify-content-between">
+            <h3> Color Generator</h3>
             <form onSubmit={handleSubmit}>
               <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" className={`${error?'error' : null }`}/>
-              <button className="btn" type="submit"> Ara </button>
+              <button className="btn" type="submit"> Search </button>
             </form>
           </section>
           <section className="colors">
